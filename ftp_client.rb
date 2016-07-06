62dc1da1985508431f56608924641b9bdc510d05
Pyer <pierre.bazonnard@gmail.com>
2016-07-06 21:49:50 +0200

    + user and url in argument

--- ftp_client.rb
+++ ftp_client.rb
@@ -3,32 +3,34 @@
 #
 require 'net/ftp'
 
+# argument is user@url:password for tutorial
+v2 = ARGV[0].split(':')
+v1 = v2[0].split('@')
 # Set these 3 variables
-ftp_url  = '???'
-ftp_user = '?'
-ftp_pass = '?'
+ftp_user = v1[0]
+ftp_url  = v1[1]
+ftp_pass = v2[1]
 
 puts "User     = #{ftp_user}"
-puts "Password = #{ftp_pass}"
 puts "URL      = #{ftp_url}"
+puts "Password = #{ftp_pass}"
 
-ftp=Net::FTP.open(ftp_url)
-ftp.login(user = ftp_user, passwd = ftp_pass)
+ftp = Net::FTP.open(ftp_url)
+ftp.login(ftp_user, ftp_pass)
 # List files in current directory
 puts ftp.list
-  # The following are the most useful FTP commands
-  # - #binary
-  # - #get
-  # - #getbinaryfile
-  # - #gettextfile
-  # - #put
-  # - #putbinaryfile
-  # - #puttextfile
-  # - #chdir
-  # - #list
-  # - #nlst
-  # - #size
-  # - #rename
-  # - #delete
-  #
+# The following are the most useful FTP commands
+#  binary
+#  get
+#  getbinaryfile
+#  gettextfile
+#  put
+#  putbinaryfile
+#  puttextfile
+#  chdir
+#  list
+#  nlst
+#  size
+#  rename
+#  delete
 ftp.close
