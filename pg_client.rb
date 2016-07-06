360544ea0df484e8aef00382a79025b746d8eca7
Pyer <pierre.bazonnard@gmail.com>
2016-07-06 21:26:03 +0200

    * compliant with Rubocop

--- pg_client.rb
+++ pg_client.rb
@@ -3,22 +3,21 @@
 #
 require 'pg'
 
-dbname='db_test'
-
-connection = PGconn.new(:dbname => "postgres", :user => 'postgres', :password => 'postgres', :host => 'localhost', :port => '5432')
-result = connection.query('SELECT datname as "Name", pg_catalog.pg_get_userbyid(datdba) as "Owner" FROM pg_catalog.pg_database ORDER BY datname;')
-connection.finish
-
+connection = PG::Connection.new(dbname: 'postgres', user: 'postgres', password: 'postgres',
+                                host: 'localhost', port: '5432')
+result = connection.query('SELECT datname as "Name", pg_catalog.pg_get_userbyid(datdba) as "Owner"
+                           FROM pg_catalog.pg_database ORDER BY datname;')
 result.each do |row|
   puts "Database #{row['Name']} is owned by #{row['Owner']}"
 end
+connection.finish
 
 puts "\nQueries:"
-connection = PGconn.new(:user => 'postgres')
-result = connection.query("select datname, query, state from pg_stat_activity;")
+connection = PG::Connection.new(user: 'postgres')
+result = connection.query('select datname, query, state from pg_stat_activity;')
 result.each do |row|
   row.each do |column|
-   p column
+    p column
   end
 end
 connection.finish
