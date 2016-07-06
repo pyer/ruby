b33ace9f904699d3deae58f623fc0f8ef708f8d5
Pyer <pierre.bazonnard@gmail.com>
2016-07-06 23:32:54 +0200

    + arguments for test

--- ssh_client.rb
+++ ssh_client.rb
@@ -3,15 +3,23 @@
 #
 require 'net/ssh'
 
-# Set these 4 variables
-host     = '?'
-user     = '?'
-password = '???'
-port     =  22 # optional if port = 22
+# arguments are 'user@host password port' for this tutorial
+user, host = ARGV[0].split('@')
+password = ARGV[1]
+if ARGV[2].nil?
+  port =  22 # optional if port = 22
+else
+  port = ARGV[2]
+end
+
+puts "User     = #{user}"
+puts "Host     = #{host}"
+puts "Password = #{password}"
+puts "Port     = #{port}"
 
 begin
-  session = Net::SSH.start(host, user, :password => password, :port => port)
-  result = session.exec!("ls -l")
+  session = Net::SSH.start(host, user, password: password, port: port)
+  result = session.exec!('ls -l')
   puts result
   session.close
 rescue Net::SSH::AuthenticationFailed
@@ -20,8 +28,8 @@ end
 
 puts '--- with block'
 begin
-  Net::SSH.start(host, user, :password => password, :port => port) do |ssh|
-    result = ssh.exec!("ls -l")
+  Net::SSH.start(host, user, password: password, port: port) do |ssh|
+    result = ssh.exec!('ls -l')
     puts result
   end
 rescue Net::SSH::AuthenticationFailed
@@ -30,11 +38,10 @@ end
 
 puts '--- with known certificate'
 begin
-  Net::SSH.start(host, user, :port => port) do |ssh|
-    result = ssh.exec!("ls -l")
+  Net::SSH.start(host, user, port: port) do |ssh|
+    result = ssh.exec!('ls -l')
     puts result
   end
 rescue Net::SSH::AuthenticationFailed
   puts 'ERROR: Authentication failed'
 end
-
