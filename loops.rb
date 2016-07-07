de7a8b02f37cca78df7d5779e7160b70a99d2eae
Pyer <pierre.bazonnard@gmail.com>
2016-07-07 17:09:25 +0200

    + using Minitest

--- loops.rb
+++ loops.rb
@@ -1,84 +1,99 @@
-#!/usr/bin/ruby
+# encoding: UTF-8
+require 'minitest/autorun'
 
-print "\nRuby loop statements"
-print "\n--------------------"
-print "\nwhile    : "
-$i = 0
-while $i < 3
-  print " #$i"
-  $i +=1
-end
-
-print "\nwhile do : "
-$i = 0
-while $i < 3 do
-  print " #$i"
-  $i +=1
-end
-
-print "\ndo while : "
-$i = 0
-begin
-  print " #$i"
-  $i += 1
-end while $i < 3
-
-print "\nuntil do : "
-$i = 0
-until $i > 2 do
-  print " #$i"
-  $i += 1
-end
-
-print "\ndo until : "
-$i = 0
-begin
-  print " #$i"
-  $i += 1
-end until $i > 2
-
-print "\nfor      : "
-for i in 0..2
-  print " #{i}"
-end
-
-print "\neach     : "
-(0..2).each do |i|
-  print " #{i}"
-end
-
-print "\n--------------------"
-
-print "\nbreak    : "
-for i in 0..7
-  print " #{i}"
-  break if i > 1
-end
-
-print "\nnext     : "
-for i in 0..3
-  next if i == 1
-  print " #{i}"
-end
-
-print "\nredo     : "
-$r=1
-for i in 0..1
-  print " #{i}"
-  $r -= 1
-  redo if i == $r
-end
-
-print "\nretry    : "
-$r=1
-begin
-  for i in 0..1
-    print " #{i}"
-    $r -= 1
-    raise if i == $r
+# Ruby loop statements tutorial
+class TestLoop < Minitest::Test
+  def test_while
+    r = ''
+    i = 0
+    while i < 3
+      r += " #{i}"
+      i += 1
+    end
+    assert_equal(r, ' 0 1 2')
   end
-rescue
-  retry
-end
 
-puts "\n--------------------"
+  def test_do_while
+    r = ''
+    i = 0
+    # Rubocop: Use Kernel#loop with break rather than begin/end/until(or while).
+    begin
+      r += " #{i}"
+      i += 1
+    end while i < 3
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_until
+    r = ''
+    i = 0
+    until i > 2
+      r += " #{i}"
+      i += 1
+    end
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_do_until
+    r = ''
+    i = 0
+    # Rubocop: Use Kernel#loop with break rather than begin/end/until(or while).
+    begin
+      r += " #{i}"
+      i += 1
+    end until i > 2
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_for
+    r = ''
+    # Rubocop: Prefer each over for.
+    for i in (0..2)
+      r += " #{i}"
+    end
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_times
+    r = ''
+    3.times do |i|
+      r += " #{i}"
+    end
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_each
+    r = ''
+    (0..2).each do |i|
+      r += " #{i}"
+    end
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_break
+    r = ''
+    (0..7).each do |i|
+      break if i > 2
+      r += " #{i}"
+    end
+    assert_equal(r, ' 0 1 2')
+  end
+
+  def test_next
+    r = ''
+    (0..3).each do |i|
+      next if i == 1
+      r += " #{i}"
+    end
+    assert_equal(r, ' 0 2 3')
+  end
+
+  def test_redo
+    r = ''
+    (0..1).each do |i|
+      r += " #{i}"
+      redo if r.length == 4
+    end
+    assert_equal(r, ' 0 1 1')
+  end
+end
